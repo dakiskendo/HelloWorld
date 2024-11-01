1cb2e1d0248781fce6e4968aafe45a0e0315714a
unknown <dakiskendo@gmail.com>
2024-11-01 14:58:36 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import HelloWorld from './Components/HelloWorld'
+function App() {
+  return (
+    <div>
+      <HelloWorld name="Davor"/>
+    </div>
+  );
+}
+
+export default App;
